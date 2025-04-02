6867f4551cb1c5e45305c021555cb7d4708608b6
fahmed10 <fahmed10@ggc.edu>
2025-04-02 18:39:30 -0400

    Add tests

--- test/FlightDataAPITests.java
+++ test/FlightDataAPITests.java
@@ -2,6 +2,8 @@ import org.junit.*;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FlightDataAPITests {
     private static FlightDataAPI flightDataAPI;
@@ -21,9 +23,34 @@ public class FlightDataAPITests {
         String url = flightDataAPI.buildUrl(
                 "Atlanta", "Cancun",
                 LocalDate.of(2025, Month.DECEMBER, 2),
-                LocalDate.of(2026, Month.JANUARY, 9)
+                LocalDate.of(2026, Month.JANUARY, 9),
+                "&test=3"
         );
 
-        Assert.assertEquals("https://www.expedia.com/Flights-Search?filters=[{\"numOfStopFilterValue\":{\"stopInfo\":{\"numberOfStops\":0,\"stopFilterOperation\":\"EQUAL\"}}}]&leg1=from:Atlanta,to:Cancun,departure:12/2/2025TANYT,fromType:U,toType:U&leg2=from:Cancun,to:Atlanta,departure:1/9/2026TANYT,fromType:U,toType:U&mode=search&options=carrier:,cabinclass:,maxhops:1,nopenalty:N&passengers=adults:1,children:0,infantinlap:N&trip=roundtrip", url);
+        Assert.assertEquals("https://flights.booking.com/flights/Atlanta.CITY-Cancun.CITY/?type=ROUNDTRIP&adults=1&cabinClass=ECONOMY&children=&from=Atlanta.CITY&to=Cancun.CITY&depart=2025-12-02&return=2026-01-09&sort=CHEAPEST&travelPurpose=leisure&test=3", url);
+    }
+
+    @Test
+    public void testGetRoundTripEconomyFlights() {
+        String[] cities = { "Rome", "Milan", "Paris", "Madrid", "Amsterdam", "Singapore" };
+        for (String city : cities) {
+            LocalDate start = LocalDate.now().plusWeeks(3);
+            LocalDate end = LocalDate.now().plusWeeks(4);
+
+            List<Flight> flights = new ArrayList<>();
+            while (flights.isEmpty()) {
+                flights = flightDataAPI.getRoundTripEconomyFlights("Atlanta", city, start, end);
+                start = start.plusDays(1);
+                end = end.plusDays(1);
+            }
+
+            for (Flight flight : flights) {
+                Assert.assertEquals("Atlanta", flight.getFromCity());
+                Assert.assertEquals(city, flight.getToCity());
+                Assert.assertEquals(start, flight.getFromDate());
+                Assert.assertEquals(end, flight.getToDate());
+                Assert.assertTrue(flight.getPrice() > 0);
+            }
+        }
     }
 }
